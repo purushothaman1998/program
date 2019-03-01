f67829f5d00bef2483a5b392556aca8d06d08f93
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-01 05:22:29 -0800

    Create ex.c

---
+++ ex.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a[50],i;
+    printf("enter the string :");
+    scanf("%d",&a);
+
+   if(a[i]=='0'||'1')
+   {
+
+   i++;
+
+printf("yes");
+   }
+    else
+    {
+
+    i++;
+printf("no");
+    }
+getch();
+}
