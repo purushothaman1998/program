549ad54d33850545935199eb9b4216df55c32a65
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-01 05:42:54 -0800

    Create bec.c

---
+++ bec.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main 
+{
+num1=int(input("Enter the 1st number:"))
+num2=int(input("Enter the 2nd number:"))
+ans=num1-num2
+if(ans%2==0):
+	print("Even")
+else:
+	print("Odd")
+  }
