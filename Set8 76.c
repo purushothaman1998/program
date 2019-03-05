b171533aa1cea361904449c2170c72fc556f4f7c
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-05 10:57:31 +0530

    Create Set8 76.c

---
+++ Set8 76.c
@@ -0,0 +1,23 @@
+
+  
+#include <stdio.h>
+int main()
+{
+    int i,h,m=2;
+    scanf("%d",&nm);
+    for(i=2;i<m;i++)
+    {
+        if(m%i==0)
+        {
+            s++;
+        }
+    }
+    if(m>2)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
