b283a7afede5d1edecf8fd777f057d6f2336ad37
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-05 12:16:32 +0530

    Create Set 8 73.c

---
+++ Set 8 73.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main(void) 
+{
+	int k,l,r;
+	scanf("%d",&k);
+	scanf("%d %d",&l,&r);
+	if(k>l && k<r)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+return 0;
+}
