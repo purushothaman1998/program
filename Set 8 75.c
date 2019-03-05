65c07bf3a30e7d7328d19dd4fc4e4ef759407300
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-05 12:23:06 +0530

    Create Set 8 75.c

---
+++ Set 8 75.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+
+	char p[30];
+	int k,i;
+	scanf("%s",a);
+	=strlen(a);
+	if(p%2==0)
+	{
+  
+		a[p/2]='*';
+		a[(p/2)-1]='*';
+	}
+	else
+	{
+		a[p/2]='*';
+	}
+	printf("\n%s",a);
+	return 0;
+}
