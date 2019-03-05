3cbd593bdaa25473b4c245d62591edc7121c1aac
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-05 17:14:15 +0530

    Create Set9 82.c

---
+++ Set9 82.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+	float len,brth,area;
+	printf("enter the length and breadth values of the farm:");
+	scanf("%f %f",&len,&brth);
+	area=len*brth:
+	printf("\n%0.5f",area);
+	return 0;
+}
