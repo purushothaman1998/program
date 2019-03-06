b813b0b924cf25bb3984f12d252916ea32460b6c
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-06 07:49:37 +0530

    Create Set6 57.c

---
+++ Set6 57.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+
+{
+    int c=0,n,k,i,a[100];
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+
+  {
+     
+     c++;
+  
+  }
+    }
+    printf("%d",c);
+    return 0;
+}
