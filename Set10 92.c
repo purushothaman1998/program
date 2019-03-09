9a1235c20ae51f0190f3ec8fa09062923f4139b1
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-09 09:13:54 +0530

    Update Set10 92.c

--- Set10 92.c
+++ Set10 92.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(void) 
 {
-	int m,s[50],i,sum=0;
-	scanf("%d",&m);
+	int k,s[50],i,sum=0;
+	scanf("%d",&k);
 	for(i=0;i<m;i++)
 	{
 		scanf("%d",&s[i]);
 		sum=sum+s[i];	
 	}
 	printf("\n%d",sum);
+	
+	
 	return 0;
 }
