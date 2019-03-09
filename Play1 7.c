1ff9a0fccf572ade1815537082d4dbe5ad963cb4
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-09 12:51:01 +0530

    Create Play1 7.c

---
+++ Play1 7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+    char str[100];
+    printf("Enter the string");
+    gets(str);
+    int length=strlen(str);
+    for(int i=0;i<length;i=i+2)
+    {
+    	if(str[i]!='\0')
+    	{
+    		int temp;
+    		temp=str[i];
+    		str[i]=str[i+1];
+    		str[i+1]=temp;
+    	}
+    }
+    printf("%s",str);
+    return 0;
+}
