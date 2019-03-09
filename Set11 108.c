4c43698fae651c20e190716cfa0cb0e294b7e27d
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-09 09:49:54 +0530

    Create Set11 108.c

---
+++ Set11 108.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int n,a,d,s;
+	int N,D,A,S;
+	scanf("%d",&n);
+	scanf("%d",&a);
+	scanf("%d",&d);
+  scanf("%d",&s);
+	N=n-1;
+	D=N*d;
+	A=2*a;
+	S=A+D;
+	s=S*n/2;
+	printf("\n%d",s);
+	return 0;
+}
