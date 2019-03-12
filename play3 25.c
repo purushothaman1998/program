1cabe93ea95add161967f056c1d97b69ada4dd30
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-12 11:54:38 +0530

    Create play3 25.c

---
+++ play3 25.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+voi dmain()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+{
+for(i=0;i<10:i++)
+printf("enter the acending numbers");
+scanf("%d",&acsending number");
+{
+else
+printf("descending number")
+}
+return 0;
+}
+}
