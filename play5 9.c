913ee7d89ce865b6c0859186513a9d4d7ce8681f
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-17 05:56:26 -0700

    Create play5 9.c

---
+++ play5 9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	signed int n;
+	scanf("%d",&n);
+	if(n>=-32768 && n<=32767)
+	{
+		printf("INT");
+	}
+	else
+	{
+		printf("LONG LONG");
+	}
+	return 0;
+}
