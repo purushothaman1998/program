af6b41390b35c9c7f1c222c3411ee74a9727cf21
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-17 04:46:29 -0700

    Create play5 4.c

---
+++ play5 4.c
@@ -0,0 +1,22 @@
+#include <iostream.h>
+ #include <conio.h>
+ #include <math.h>
+
+ main()
+    {
+       clrscr();
+
+       int x;
+
+       floatangle;
+
+       cout<<"\n Enter the value of x = ";
+       cin>>x;
+
+       amgle=0(x);
+
+       cout<<" 0("<<x<<") = "<<value<<endl;
+
+       getch ();
+       return 0;
+    }
