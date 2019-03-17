e4fe92592ba48407df21ccbe0f4471c68a0d4cce
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-16 19:56:32 -0700

    Create play5 1.c

---
+++ play5 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+{
+	int n,k,i,res=1;
+	scanf("%d %d",&n,&k);
+	for(i=1;i<=n;i++)
+	{
+	    res=res*k;
+	    if(res==n)
+	    {
+	        printf("yes..its the power of %d\n",k);
+	        break;
+	    }
+	}
+	if(res>n)
+	{
+	    printf("no");
+	}
+	return 0;
+}
