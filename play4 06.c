a64e58837abea682835085513afd6afc745bb46d
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-16 19:38:07 -0700

    Create play4 06.c

---
+++ play4 06.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	int n,k,count=0,rem;
+	scanf("%d %d",&n,&k);
+	while(n!=0)
+	{
+		rem=n%10;
+		if(rem==k)
+		{
+			count=count+1;
+		}
+		n=n/10;
+	}
+	printf("%d",count);
+	return 0;
+}
